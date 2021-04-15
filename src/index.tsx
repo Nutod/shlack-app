c9c30cbe7b5b52d450c97371145b459cbbd1ead4
Nutod <alukodotun@gmail.com>
2021-04-15 14:52:25 +0700

    Moved style config around

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.scss'
 import App from './App'
+
+import './styles/main.scss'
+
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
