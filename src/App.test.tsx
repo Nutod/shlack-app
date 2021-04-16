bb7ece319dbf3f9938799e431e49c0bbc4b1aba8
Nutod <alukodotun@gmail.com>
2021-04-16 12:02:45 +0700

    Removed unused variable

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import App from './App'
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders a link', () => {
-  render(<App />)
-  const headerElement = screen.getByText(/shlack app/i)
-  expect(headerElement).toBeInTheDocument()
-})
+test("renders a link", () => {
+  render(<App />);
+  const headerElement = screen.getByText(/shlack app/i);
+  expect(headerElement).toBeInTheDocument();
+});
