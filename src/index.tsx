60d87888b8d532c523a50fecbf1168dc4a758e1f
Nutod <alukodotun@gmail.com>
2021-04-16 11:46:27 +0700

    Removed unused files

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-// import './styles/main.scss'
-
 import reportWebVitals from './reportWebVitals'
 import { GlobalStyles } from './styles/global'
 
